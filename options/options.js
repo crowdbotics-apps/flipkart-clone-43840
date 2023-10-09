39f1b12c733d45d223d619ed900f5093c3a353af
Crowdbotics <team@crowdbotics.com>
2023-10-09 14:49:24 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "flipkart-clone-43840",
+  url: "https://flipkart-clone-43840.botics.co",
+  api: "https://flipkart-clone-43840.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
